8e2bcf93bddc870b4d43a2c8705bd5d3c186730a
Alexander Birk <birk@pingworks.de>
2015-09-29 21:08:43 +0000

    inital commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ws-testhelper'
+maintainer       'Alexander Birk'
+maintainer_email 'birk@pingworks.de'
+license          'Apache 2.0'
+description      'Installs/Configures ws-testhelper'
+long_description 'Installs/Configures ws-testhelper'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: ws-testhelper
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+node.default['ws-testhelper']['directory'] = "/tmp/serverspec-test"
+node.default['ws-testhelper']['file_name'] = "node.json"
+
+directory node.default['ws-testhelper']['directory'] do
+  recursive true
+end
+
+ruby_block "dump_node_attributes" do
+  block do
+    require 'json'
+    attrs = {}
+
+    attrs = Chef::Mixin::DeepMerge.deep_merge(node.default_attrs, attrs) unless node.default_attrs.empty?
+    attrs = Chef::Mixin::DeepMerge.deep_merge(node.normal_attrs, attrs) unless node.normal_attrs.empty?
+    attrs = Chef::Mixin::DeepMerge.deep_merge(node.override_attrs, attrs) unless node.override_attrs.empty?
+
+    recipe_json = "{ \"run_list\": \[ "
+    recipe_json << node.run_list.expand(node.chef_environment).recipes.map! { |k| "\"#{k}\"" }.join(",")
+    recipe_json << " \] }"
+    attrs = Chef::Mixin::DeepMerge.deep_merge(JSON.parse(recipe_json), attrs)
+
+    File.open("#{node.default['ws-testhelper']['directory']}/#{node.default['ws-testhelper']['file_name']}", 'w') { |file| file.write(JSON.pretty_generate(attrs)) }
+  end
+end
+
